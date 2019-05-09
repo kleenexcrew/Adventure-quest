c4a7c091fc56fd4a4250c1245d6d30efa52949ce
Wes Griffin <themisguidedidiot@gmail.com>
2019-05-09 14:10:25 -0700

    made has completed function

--- test/has-completed-all-quests.test.js
+++ test/has-completed-all-quests.test.js
@@ -1,10 +1,10 @@
 import hasCompletedAllQuests from '../src/map/has-completed-all-quests.js';
 const test = QUnit.test;
 
+const quests = [{ id: 'quest1' }, { id: 'quest2' }, { id: 'quest3' }];
 
 test('return true when completed all quests', assert => {
     // Arrange
-    const quests = [{ id: 'quest1' }, { id: 'quest2' }, { id: 'quest3' }];
     const user = { 
         completed: {
             quest1: true,
@@ -17,4 +17,18 @@ test('return true when completed all quests', assert => {
     const result = hasCompletedAllQuests(quests, user);
     // Assert
     assert.equal(result, expected);
+});
+
+test('return false if user hasnt completed all quests', assert => {
+    //arrange
+    const user = {
+        completed: {
+            quest1: true,
+            quest2: true
+        }
+    };
+    const expected = false;
+    //act
+    const result = hasCompletedAllQuests(quests, user);
+    assert.equal(result, expected);    
 });
