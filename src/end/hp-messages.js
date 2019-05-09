19be87fa619865e3a27a924393230d2e4ed8247b
Wes Griffin <themisguidedidiot@gmail.com>
2019-05-09 13:38:40 -0700

    made end html and messages for end

---
+++ src/end/hp-messages.js
@@ -0,0 +1,7 @@
+const hpMessages = {
+    dead: 'You Died!',
+    crippled: 'You made it out alive...but you are crippled!',
+    healthy: 'You are in prime condition!'
+};
+
+export default hpMessages;
