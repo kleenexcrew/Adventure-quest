c3f09a0b180f900cd9c2c0a6880cfa595258c4b9
Wes Griffin <themisguidedidiot@gmail.com>
2019-05-09 15:24:27 -0700

    populated end.html witht results data

--- src/end/end.js
+++ src/end/end.js
@@ -1,3 +1,20 @@
 import loadProfile from '../load-profile.js';
+import scoreCs from './score-cs.js';
+import scoreHp from './score-hp.js';
+import api from '../services/api.js';
+import csMessages from './cs-messages.js';
+import hpMessages from './hp-messages.js';
+
+const user = api.getUser();
+const hpImage = document.getElementById('hp-image');
+const hpMessage = document.getElementById('hp-message');
+const csImage = document.getElementById('cs-image');
+const csMessage = document.getElementById('cs-message');
+
+const csString = csMessages[scoreCs(user)];
+const hpString = hpMessages[scoreHp(user)];
+
+hpMessage.textContent = hpString;
+csMessage.textContent = csString;
 
 loadProfile();
